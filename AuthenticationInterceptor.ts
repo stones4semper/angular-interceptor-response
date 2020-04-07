cd0c6d7e0ca718289530564ecdf31871860cf23f
Eloike David <eloike95@gmail.com>
2020-04-07 02:54:14 +0100

    Update AuthenticationInterceptor.ts

--- AuthenticationInterceptor.ts
+++ AuthenticationInterceptor.ts
@@ -1,3 +1,7 @@
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse, HttpErrorResponse }   from '@angular/common/http';
+import { Injectable } from "@angular/core"
+import { Observable, of } from "rxjs";
+import { tap, catchError } from "rxjs/operators";
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
   constructor(
